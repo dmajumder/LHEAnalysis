a6b2176eb797b6ad3ed36f8a375f88d3ada09744
Devdatta <devdatta.majumder@cern.ch>
2015-04-10 17:24:11 +0200

    Additional histos for W from t and l from W

--- bin/SingleBprime.cc
+++ bin/SingleBprime.cc
@@ -142,6 +142,16 @@ void readerExample(std::string& fname) {
 
   fout.str(std::string());
   fout.clear() ;
+  fout << fnamecut << ";p_{T} (W) [GeV];" ;
+  TH1D* hnpart9_pt_W = new TH1D("hnpart9_pt_W" ,(fout.str()).c_str() ,200 ,0.  ,1000) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
+  fout << fnamecut << ";y (W);" ;
+  TH1D* hnpart9_y_W  = new TH1D("hnpart9_y_W"  ,(fout.str()).c_str() ,100 ,-5 , 5) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
   fout << fnamecut << ";p_{T} (H) [GeV];" ;
   TH1D* hnpart9_pt_H = new TH1D("hnpart9_pt_H" ,(fout.str()).c_str() ,200 ,0.  ,1000) ; 
 
@@ -182,6 +192,26 @@ void readerExample(std::string& fname) {
 
   fout.str(std::string());
   fout.clear() ;
+  fout << fnamecut << ";p_{T} (b from t) [GeV];" ;
+  TH1D* hnpart9_pt_bfromt = new TH1D("hnpart9_pt_bfromt" ,(fout.str()).c_str() ,200 ,0.  ,1000) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
+  fout << fnamecut << ";y (b from t);" ;
+  TH1D* hnpart9_y_bfromt  = new TH1D("hnpart9_y_bfromt"  ,(fout.str()).c_str() ,100 ,-5 , 5) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
+  fout << fnamecut << ";p_{T} (b from t) [GeV];" ;
+  TH1D* hnpart9_pt_lfromw = new TH1D("hnpart9_pt_lfromw" ,(fout.str()).c_str() ,200 ,0.  ,1000) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
+  fout << fnamecut << ";y (b from t);" ;
+  TH1D* hnpart9_y_lfromw  = new TH1D("hnpart9_y_lfromw"  ,(fout.str()).c_str() ,100 ,-5 , 5) ; 
+
+  fout.str(std::string());
+  fout.clear() ;
 
   while ( reader1.readEvent() ) {
     int npart(0) ; 
@@ -193,55 +223,71 @@ void readerExample(std::string& fname) {
           sqrt( pow2(reader1.hepeup.PUP[ii][0]) + pow2(reader1.hepeup.PUP[ii][1]) ) > 0 ) hardpart = true ;
     }
     if ( !hardpart ) continue ; 
-    TLorentzVector p4_tp, p4_t, p4_H, p4_b, p4_q ; 
+    TLorentzVector p4_tp, p4_t, p4_H, p4_b, p4_q, p4_W, p4_bfromt, p4_lfromw ; 
     for(int ii=0;ii< reader1.hepeup.NUP;ii++){
       if ( reader1.hepeup.IDUP[ii] == 8000001 ) {
         p4_tp.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
         if ( npart == 6 ) hnpart6_pt_tp->Fill(p4_tp.Pt()) ; 
         if ( npart == 6 ) hnpart6_y_tp -> Fill(p4_tp.Rapidity()) ; 
-        if ( npart == 9 ) hnpart9_pt_tp->Fill(p4_tp.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_tp -> Fill(p4_tp.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_tp->Fill(p4_tp.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_tp -> Fill(p4_tp.Rapidity()) ; 
       }
       else if ( reader1.hepeup.IDUP[ii] == -8000001 ) {
         p4_tp.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
         if ( npart == 6 ) hnpart6_pt_tpbar->Fill(p4_tp.Pt()) ; 
         if ( npart == 6 ) hnpart6_y_tpbar -> Fill(p4_tp.Rapidity()) ; 
-        if ( npart == 9 ) hnpart9_pt_tpbar->Fill(p4_tp.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_tpbar -> Fill(p4_tp.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_tpbar->Fill(p4_tp.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_tpbar -> Fill(p4_tp.Rapidity()) ; 
       }
       else if ( reader1.hepeup.IDUP[ii] == 6 && reader1.hepeup.MOTHUP[ii].first ==  3 && reader1.hepeup.MOTHUP[ii].second ==  3 ) {
         p4_t.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
-        if ( npart == 9 ) hnpart9_pt_t->Fill(p4_t.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_t -> Fill(p4_t.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_t->Fill(p4_t.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_t -> Fill(p4_t.Rapidity()) ; 
       }
       else if ( reader1.hepeup.IDUP[ii] == -6 && reader1.hepeup.MOTHUP[ii].first == 3 && reader1.hepeup.MOTHUP[ii].second ==  3 ) {
         p4_t.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
-        if ( npart == 9 ) hnpart9_pt_tbar->Fill(p4_t.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_tbar -> Fill(p4_t.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_tbar->Fill(p4_t.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_tbar -> Fill(p4_t.Rapidity()) ; 
       }
       else if ( abs(reader1.hepeup.IDUP[ii]) == 25 && reader1.hepeup.MOTHUP[ii].first ==  3 && reader1.hepeup.MOTHUP[ii].second ==  3 ) {
         p4_H.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
-        if ( npart == 9 ) hnpart9_pt_H->Fill(p4_H.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_H -> Fill(p4_H.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_H->Fill(p4_H.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_H -> Fill(p4_H.Rapidity()) ; 
       }
       else if ( reader1.hepeup.IDUP[ii] == 5 && reader1.hepeup.MOTHUP[ii].first ==  1 && reader1.hepeup.MOTHUP[ii].second ==  2 ) {
         p4_b.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
         if ( npart == 6 ) hnpart6_pt_b->Fill(p4_b.Pt()) ; 
         if ( npart == 6 ) hnpart6_y_b -> Fill(p4_b.Rapidity()) ; 
-        if ( npart == 9 ) hnpart9_pt_b->Fill(p4_b.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_b -> Fill(p4_b.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_b->Fill(p4_b.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_b -> Fill(p4_b.Rapidity()) ; 
       }
       else if ( reader1.hepeup.IDUP[ii] == -5 && reader1.hepeup.MOTHUP[ii].first == 1 && reader1.hepeup.MOTHUP[ii].second ==  2 ) {
         p4_b.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
         if ( npart == 6 ) hnpart6_pt_bbar->Fill(p4_b.Pt()) ; 
         if ( npart == 6 ) hnpart6_y_bbar -> Fill(p4_b.Rapidity()) ; 
-        if ( npart == 9 ) hnpart9_pt_bbar->Fill(p4_b.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_bbar -> Fill(p4_b.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_bbar->Fill(p4_b.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_bbar -> Fill(p4_b.Rapidity()) ; 
       }
       else if ( abs(reader1.hepeup.IDUP[ii]) < 5 && reader1.hepeup.MOTHUP[ii].first ==  1 && reader1.hepeup.MOTHUP[ii].second ==  2 ) {
         p4_q.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
-        if ( npart == 9 ) hnpart9_pt_q->Fill(p4_q.Pt()) ; 
-        if ( npart == 9 ) hnpart9_y_q -> Fill(p4_q.Rapidity()) ; 
+        if ( npart == 11 ) hnpart9_pt_q->Fill(p4_q.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_q -> Fill(p4_q.Rapidity()) ; 
+      }
+      else if ( abs(reader1.hepeup.IDUP[ii]) == 24 && reader1.hepeup.MOTHUP[ii].first ==  4 && reader1.hepeup.MOTHUP[ii].second ==  4 ) {
+        p4_W.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
+        if ( npart == 11 ) hnpart9_pt_W->Fill(p4_W.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_W -> Fill(p4_W.Rapidity()) ; 
+      }
+      else if ( abs(reader1.hepeup.IDUP[ii]) == 5 && reader1.hepeup.MOTHUP[ii].first ==  4 && reader1.hepeup.MOTHUP[ii].second ==  4 ) {
+        p4_bfromt.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
+        if ( npart == 11 ) hnpart9_pt_bfromt->Fill(p4_bfromt.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_bfromt -> Fill(p4_bfromt.Rapidity()) ; 
+      }
+      else if (  ( abs(reader1.hepeup.IDUP[ii]) == 11 || abs(reader1.hepeup.IDUP[ii]) == 13 || abs(reader1.hepeup.IDUP[ii]) == 15 )  
+          && reader1.hepeup.MOTHUP[ii].first ==  5 && reader1.hepeup.MOTHUP[ii].second ==  5 ) {
+        p4_lfromw.SetPxPyPzE(reader1.hepeup.PUP[ii][0], reader1.hepeup.PUP[ii][1], reader1.hepeup.PUP[ii][2], reader1.hepeup.PUP[ii][3]) ; 
+        if ( npart == 11 ) hnpart9_pt_lfromw->Fill(p4_lfromw.Pt()) ; 
+        if ( npart == 11 ) hnpart9_y_lfromw -> Fill(p4_lfromw.Rapidity()) ; 
       }
     }
 
